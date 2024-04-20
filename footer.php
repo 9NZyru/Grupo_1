c174dae185f8000f26ab8830600af92b71a84fe9
Christopher Santana <christophersantana222>
2024-04-20 13:22:43 -0400

    agregamos todos los archivos

---
+++ footer.php
@@ -0,0 +1,39 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Enlaces rápidos</h3>
+         <a href="home.php">Inicio</a>
+         <a href="about.php">Acerca de</a>
+         <a href="shop.php">Comercio</a>
+         <a href="contact.php">Contacto</a>
+      </div>
+
+      <div class="box">
+         <h3>enEaces adicionales</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">Registro</a>
+         <a href="cart.php">Carrito</a>
+         <a href="orders.php">Pedidos</a>
+      </div>
+
+      <div class="box">
+         <h3>Datos de contacto</h3>
+         <p> <i class="fas fa-phone"></i> +828-221-3571 </p>
+         <p> <i class="fas fa-phone"></i> +829-356-7313 </p>
+         <p> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Jacagua, República Dominicana - 51000 </p>
+      </div>
+
+      <div class="box">
+         <h3>Síganos</h3>
+         <a href="https://www.facebook.com/share/cWyY4upJ5QuhXMzx/?mibextid=WC7FNe"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="https://whatsapp.com/channel/0029VaaJ6Ep8qIzuv3Iuru3a"> <i class="fa-WhatsApp"></i> WhatsApp </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; Grupo-1  @ <?php echo date('Y'); ?> por <span>Christopher, Génesis, Arianni, Darisbel, Kimberly</span> </p>
+
+</section>
